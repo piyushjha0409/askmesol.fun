9391648411c94890cf6e44d376d17afe0b5c2143
piyushjha0409 <peeyush0409@gmail.com>
2024-11-01 23:27:22 +0530

    Feat: Implemented the prisma and now ready to test

--- auth.ts
+++ auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
 import Twitter from "next-auth/providers/twitter";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Twitter, Google],
 });
